db4b9161ee82c038dfb69d681d00fa2ce19ec864
sen <995725622@qq.com>
2018-09-11 16:54:54 +0800

    dao and mapper

--- src/com/project/service/ITecInterViewService.java
+++ src/com/project/service/ITecInterViewService.java
@@ -15,22 +15,17 @@ public interface ITecInterViewService {
 	/**
 	 * 显示学员信息，参数用来判断要显示学员的范围
 	 * @param status 值：全部、通过、未通过
-	 * @return
-	 */
-	public List<StudentBean> showStudent(String status);
-	
-	/**
-	 * 通过条件查找学生
-	 * @param status 值：全部、通过、未通过
 	 * @param bean 学生实体用来保存查询条件
 	 * @return
 	 */
-	public StudentBean findStudent(String status,StudentBean bean);
+	public List<StudentBean> showStudent(String status,StudentBean bean);
+	
 	
 	/**
 	 * 面试
 	 * @param bean 技术面试实体
 	 */
 	public void interview(TecInterViewBean bean);
+
 	
 }
